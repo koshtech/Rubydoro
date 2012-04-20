c08a4b308bec0d6bf19c7a0f0e4abda352d54e5d
Vinícius Rodrigues <vini.rodrigues.90@gmail.com>
2012-04-20 11:16:24 -0300

    Aplicativo Shoes #1857

--- rubydoro.rb
+++ rubydoro.rb
@@ -1,34 +1,40 @@
-Shoes.app :title => "Rubydoro" do
+Shoes.app :title => "Rubydoro", :width => 300, :height => 160 do
   background darkred
 
   @main = stack do
-    title code("Rubydoro"), :stroke => white 
-    @start = button "Start your day"
+    title code("Rubydoro"), :stroke => white, :margin_left => 40, :margin_top => 20
+    @start = button "Start your day", :margin_left => 80, :margin_top => 10
   end
 
   @start.click do
-    window :title => "Plan your day" do
+    window :title => "Plan your day", :width => 850, :height => 730 do
       background darkred
-      title "Things to do today", :stroke => white
-      
-      @task = edit_line :width => 230, :margin_left => 10
+      flow do
+        title "Things to do today", :stroke => white
+      end
+      @task = edit_line :width => 400, :margin_left => 10, :margin_top => 10
       @counter = 0
-      button "Add" do
+      button "Add", :margin_left => 5, :margin_top => 10 do
         stack do
           @f = flow do
+            flow do
+              para "------------------------------------------------------------------------------------------------------------------------------------------------------------------------", :stroke => white
+            end
             para "#{@counter += 1} ~> ", :stroke => white
             para @task.text, :stroke => white
             para " "
             @task.text = ""
             button "Go Pomodoro" do
-              window :title => "Pomodoro" do
+              window :title => "Pomodoro", :width => 300, :height => 180 do
                 background darkred
                 @seconds = 1500
                 @paused = false
 
                 def display_time
                   @display.clear do
-                    title "%02d:%02d" % [@seconds / 60 % 60, @seconds % 60] , :stroke => @paused ? gray : white
+                    title "%02d:%02d" % [@seconds / 60 % 60, @seconds % 60],
+                    :stroke => @paused ? gray : white,
+                    :margin_left => 95                    
                   end
                 end
 
@@ -36,17 +42,19 @@ Shoes.app :title => "Rubydoro" do
                 display_time
 
                 button "Interruption", :width => '100%' do
+                  if @paused == false
+                    para "* ", :stroke => white, :margin_top => 10
+                  end
                   @paused = true
                   display_time 
-                  para "* ", :stroke => white
                 end
         
-                button "Go", :width => '100%' do
+                button "Back to work", :width => '100%' do
                   @paused = false
                   display_time
                 end
 
-                para "Interruptions: ", :stroke => white
+                para "Interruptions: ", :stroke => white, :margin_top => 10
 
                 animate(1) do
                   @seconds -= 1 unless (@paused || @seconds == 0)
@@ -57,14 +65,16 @@ Shoes.app :title => "Rubydoro" do
             end
 
             button "Short Break" do
-              window :title => "Short Break" do
+              window :title => "Short Break", :width => 300, :height => 180 do
                 background darkred
                 @sbtime = 300
                 @p = false
 
                 def display_timesb
                   @displaysb.clear do
-                    title "%02d:%02d" % [@sbtime / 60 % 60, @sbtime % 60] , :stroke => @p ? gray : white
+                    title "%02d:%02d" % [@sbtime / 60 % 60, @sbtime % 60],
+                    :stroke => @p ? gray : white,
+                    :margin_left => 95 
                   end
                 end
 
@@ -76,7 +86,7 @@ Shoes.app :title => "Rubydoro" do
                   display_timesb
                 end
         
-                button "Go", :width => '100%' do
+                button "Continue break", :width => '100%' do
                   @p = false
                   display_timesb
                 end
@@ -89,14 +99,16 @@ Shoes.app :title => "Rubydoro" do
             end 
 
             button "Long Break" do
-              window :title => "Long Break" do
+              window :title => "Long Break", :width => 300, :height => 180 do
                 background darkred
                 @lbtime = 1800
                 @p2 = false
 
                 def display_timelb
                   @displaylb.clear do
-                    title "%02d:%02d" % [@lbtime / 60 % 60, @lbtime % 60] , :stroke => @p2 ? gray : white
+                    title "%02d:%02d" % [@lbtime / 60 % 60, @lbtime % 60],
+                    :stroke => @p2 ? gray : white,
+                    :margin_left => 95 
                   end
                 end
 
@@ -108,7 +120,7 @@ Shoes.app :title => "Rubydoro" do
                   display_timelb
                 end
         
-                button "Go", :width => '100%' do
+                button "Continue break", :width => '100%' do
                   @p2 = false
                   display_timelb
                 end
@@ -124,6 +136,9 @@ Shoes.app :title => "Rubydoro" do
               @f.clear
             end
 
+            flow do
+              para "------------------------------------------------------------------------------------------------------------------------------------------------------------------------", :stroke => white
+            end
           end
         end
       end
